8eef0b5d05a0f725eeb1beae1da2f90f7fb43307
David Kay <davidbkay@gmail.com>
2020-01-30 09:04:39 -0600

    init commit

---
+++ layouts/default.vue
@@ -0,0 +1,150 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      dark
+      color="deep-purple darken-3"
+    >
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn @click.stop="miniVariant = !miniVariant" icon>
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn @click.stop="clipped = !clipped" icon>
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-btn @click.stop="fixed = !fixed" icon>
+        <v-icon>mdi-minus</v-icon>
+      </v-btn> -->
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn @click.native="locateMe" icon>
+        <v-icon>mdi-crosshairs-gps</v-icon>
+      </v-btn>
+      <v-btn @click.stop="rightDrawer = !rightDrawer" icon>
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-snackbar
+      v-model="snackbar"
+      multi-line
+      :color="errorStr ? 'red' : 'green'"
+    >
+      <span v-if="errorStr">
+        {{ errorStr }}
+      </span>
+      <span v-else-if="location">
+        {{
+          `Your location data is ${location.coords.latitude}, ${location.coords.longitude}`
+        }}
+      </span>
+    </v-snackbar>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      location: null,
+      snackbar: false,
+      gettingLocation: false,
+      errorStr: null,
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'TEC'
+    }
+  },
+  methods: {
+    async getLocation() {
+      await new Promise((resolve, reject) => {
+        if (!('geolocation' in navigator)) {
+          reject(new Error('Geolocation service is not available.'))
+        }
+
+        navigator.geolocation.getCurrentPosition(
+          (pos) => {
+            this.snackbar = true
+            resolve(pos)
+          },
+          (err) => {
+            this.snackbar = true
+            reject(err)
+          }
+        )
+      })
+    },
+    async locateMe() {
+      this.gettingLocation = true
+      try {
+        this.gettingLocation = false
+        this.location = await this.getLocation()
+      } catch (e) {
+        this.gettingLocation = false
+        this.errorStr = e.message
+      }
+    }
+  }
+}
+</script>
